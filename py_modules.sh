1127abf84ee6cd3d44fadbb821739f74e8e74f1f
Tyler <frogworks1@gmail.com>
2016-05-18 15:49:19 -0700

    Update py_modules.sh

--- py_modules.sh
+++ py_modules.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
 echo Installing Requests python module
 pip install requests
+echo Requests module install complete
+
 echo Installing Scrappy python module
+pip install scrappy
+echo Scrappy module install complete
+
+echo Installing Pillow python module
+pip install Pillow
+echo Pillow module install complete
+
+echo Installing SQLAlchemy python module
+pip install SQLAlchemy
+echo SQLAlchemy module install complete
+
+echo Installing BeautifulSoup python module
+pip install BeautifulSoup
+echo BeautifulSoup module install complete
+
+echo Installing twisted python module
+pip install twisted
+echo twisted module install complete
+
+echo Installing NumPy python module
+pip install NumPy
+echo NumPy module install complete
+
+echo Installing pyglet python module
+pip install pyglet
+echo pyglet module install complete
